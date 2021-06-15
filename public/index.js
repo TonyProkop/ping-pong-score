1b9d2d7d1702b479bac756d2b6eb082a2051d5a6
Tony Prokop <prokop.tony@gmail.com>
2021-06-15 16:49:40 -0500

    Added up/down buttons.

--- public/index.js
+++ public/index.js
@@ -6,3 +6,32 @@ document.addEventListener("DOMContentLoaded", function() {
 		document.getElementById('team-b-score').innerHTML = data.teamBScore
 	});
 })
+
+const teamAUpButton = document.querySelector('.team-a .up-button');
+const teamADownButton = document.querySelector('.team-a .down-button');
+const teamBUpButton = document.querySelector('.team-b .up-button');
+const teamBDownButton = document.querySelector('.team-b .down-button');
+
+teamAUpButton.addEventListener("click", function () {
+	fetch('/a/up', {
+		method: 'PUT'
+	})
+})
+
+teamADownButton.addEventListener("click", function () {
+	fetch('/a/down', {
+		method: 'PUT'
+	})
+})
+
+teamBUpButton.addEventListener("click", function () {
+	fetch('/b/up', {
+		method: 'PUT'
+	})
+})
+
+teamBDownButton.addEventListener("click", function () {
+	fetch('/b/down', {
+		method: 'PUT'
+	})
+})
